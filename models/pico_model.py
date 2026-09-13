24bca41d97376d237e25694652089ec4f560dde5
kaleemmr <kaleemrahman05@gmail.com>
2026-09-13 17:55:21 +0100

    Update pico_model.py

--- models/pico_model.py
+++ models/pico_model.py
@@ -1,16 +1,13 @@
-# Auto-generated Micro-ML Decision Rules for MicroPython
-# Derived from Scikit-Learn IsolationForest Model Binary
+
 
 TEMP_MIN = 21.32
-TEMP_MAX = 22.48
+TEMP_MAX = 22.48     # models temperature values obtained from raw data before
 HUM_MIN = 56.56
-HUM_MAX = 65.02
+HUM_MAX = 65.02      # models humidity values obtained from raw data before
 
-def predict(temp, hum):
-    """
-    Returns 1 for NORMAL, -1 for ANOMALY based on trained tree thresholds.
-    """
+def predict(temp, hum): # obtains the raw inputs for temperature and humidity from the sensor.
+    
     if (TEMP_MIN <= temp <= TEMP_MAX) and (HUM_MIN <= hum <= HUM_MAX):
-        return 1  # Normal
+        return 1  # returns 1 if values obtained are within thresholds and are normal.
     else:
-        return -1 # Anomaly
+        return -1 # returns -1 if values obtained are not within thresholds and are an anomaly.
